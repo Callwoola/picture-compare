6ebc8b5f9ea11ed84cc9c583de95c17827bc0f65
Neo <call@woola.net>
2015-07-09 16:10:42 +0800

    Modified Controller

--- controller/demo.py
+++ controller/demo.py
@@ -3,33 +3,29 @@ import tornado.ioloop
 import tornado.web
 import tornado.template
 from lib import Image
+from lib import Manage
 from lib import Compare
 
-PROJECT_DIR = "/Users/liyang/Code/pictureCompare/"
+PROJECT_DIR = "D:/code/image/"
 STATIC_DIR = "img/"
 PROJECT_DIR_IMG = PROJECT_DIR + "img/"
-
 COMPARE_IMG = "1.jpg"
+TEMPLATE_DIR = PROJECT_DIR + "template/"
 
-
-class MainHandler(tornado.web.RequestHandler):
-
+class DemoHandler(tornado.web.RequestHandler):
     def get(self):
-        loader = tornado.template.Loader(PROJECT_DIR+"web/")
+        loader = tornado.template.Loader(TEMPLATE_DIR)
         image = Image.Image()
         compare = Compare.Image()
-        packages = ["img1/", "img2/","img4/"]
-        #packages = ["img1/", "img2/"]
-
-        #packages = ["img3/"]
-
+        packages = ["img1/", "img2/", "img4/"]
+        manage=Manage.Manage()
         package_image = []
         for package in packages:
             # --------------------------
             # get differ image package
             # package = "img1/"
-            images = image.getImageList(PROJECT_DIR_IMG + package, STATIC_DIR + package)
-            COMPARE_IMG = image.getFirst(PROJECT_DIR_IMG + package)
+            images = manage.getImageList(PROJECT_DIR_IMG + package, STATIC_DIR + package)
+            COMPARE_IMG = manage.getFirst(PROJECT_DIR_IMG + package)
             print images
             for i in range(0, len(images)):
                 compare.setA(PROJECT_DIR_IMG + package + COMPARE_IMG)
@@ -38,18 +34,57 @@ class MainHandler(tornado.web.RequestHandler):
                 images[i]['origin_img'] = STATIC_DIR + package + COMPARE_IMG
 
                 compare.start()
-                images[i]['code'] = compare.base()
+                images[i]['code'] = compare.phash()
                 images[i]['code2'] = compare.mse()
                 images[i]['code3'] = compare.perceptualHash()
+                images[i]['color'] = image.getRgbaString(images[i]['path'])
                 compare.end()
                 # images[i]['code3'] = Compare.correlate2d()
             package_image.append(images)
 
         print package_image
-        html = loader.load("index.html") \
+        html = loader.load("demo.html") \
             .generate(images=images,
                       packages=packages,
                       package_image=package_image,
                       COMPARE_IMG=PROJECT_DIR_IMG + COMPARE_IMG
                       )
         self.write(html)
+
+
+class DemoSearchHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write(tornado.template.Loader(TEMPLATE_DIR).load("search.html").generate(
+            action='upload_search'
+        ))
+"""
+TODO search test
+"""
+class DemoUploadSearchHandler(tornado.web.RequestHandler):
+    def post(self):
+        import StringIO
+        from PIL import Image
+        from lib import Manage
+        try:
+            imgfiles = self.request.files['file_img']
+            if len(imgfiles)>1:
+                return self.write("error")
+            imgfile = imgfiles[0]
+            filename=imgfile['filename']
+            Image.open(StringIO.StringIO(imgfile['body'])).save(filename)
+
+            path=PROJECT_DIR+"tests/img/img1/"
+            # start compare imgs
+            manage=Manage.Manage()
+            result=[]
+            for i in manage.getPathAllFile(path):
+                print result.append({
+                    ''
+                })
+            self.write(tornado.template.Loader(TEMPLATE_DIR).load("search_result.html").generate(
+                action='upload_search',
+                result=result
+            ))
+        except Exception,e:
+            print e
+            self.redirect('search')
