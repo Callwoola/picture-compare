8d357bc9db76a85ac1e9b72eadfb1bd1589a9aaf
Neo <call@woola.net>
2015-07-09 16:14:34 +0800

    Added test file

---
+++ controller/search.py
@@ -0,0 +1,23 @@
+import tornado
+import tornado.ioloop
+import tornado.web
+import tornado.template
+from lib import Image
+from lib import Compare
+
+PROJECT_DIR = "D:/code/image/"
+STATIC_DIR = "img/"
+PROJECT_DIR_IMG = PROJECT_DIR + "img/"
+
+COMPARE_IMG = "1.jpg"
+
+
+class SearchHandler(tornado.web.RequestHandler):
+
+    def get(self):
+
+        # first to decide the picture type by histogram
+
+        # is data is  Smooth  that is material else
+
+        self.write('{error:404}')
