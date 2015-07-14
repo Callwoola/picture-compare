65f21863ecbc3bd24308fcd3a775dbe05b4f4c34
Neo <call@woola.net>
2015-07-14 09:57:53 +0800

    Modfied Change encode

--- src/controller/compare.py
+++ src/controller/compare.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 import tornado
 import tornado.ioloop
 import tornado.web
@@ -7,52 +7,56 @@ import json
 from src.module import json_module
 from src.adapter import db
 
+
 class pcHandler(tornado.web.RequestHandler):
     """
     picture compare
     RESTFUL api style
     """
-    def get(self,type):
+    def get(self, type):
         # db.pcDB
         self.set_header('Content-Type', 'application/json')
-        jsonM=json_module.json_module()
-        jsonM.set('data',[
-            {'material':{
-                'id':12
+        jsonM = json_module.json_module()
+        jsonM.set('data', [
+            {'material': {
+                'id': 12
             }},
-            {'material':"123123"},
-            {'material':"123123"}
+            {'material': "123123"},
+            {'material': "123123"}
         ])
-        jsonM.set('status','OK')
-        if self.get_argument("type") in ("json","path","url"):
+        jsonM.set('status', 'OK')
+        if self.get_argument("type") in ("json", "path", "url"):
             def getAll():
                 return []
+
             self.write(jsonM.get())
             return
-        self.write(jsonM.set('status','error').get())
+        self.write(jsonM.set('status', 'error').get())
 
-    def post(self,type):
+    def post(self, type):
+        self.set_header('Content-Type', 'application/json')
+        jsonM = json_module.json_module()
 
+        self.write(jsonM
+                   .set('status', 'no')
+                   .set('data', 'no')
+                   .get())
         pass
 
 
-# class JsonHandler(tornado.web.RequestHandler):
-#     """
-#     RESTFUL api style
-#     """
-#     def get(self):
-#         self.write('{error:404}')
 
 class IndexHandler(tornado.web.RequestHandler):
     """
     RESTFUL api style
     """
+
     def post(self):
         self.set_header('Content-Type', 'application/json')
-        post_str=""
-        postJson=json.load(post_str)
-        #db.index(post_str)
+        post_str = ""
+        postJson = json.load(post_str)
+        # db.index(post_str)
         pass
+
     def get(self, *args, **kwargs):
         self.set_header('Content-Type', 'application/json')
         self.write('{error:404}')
--- src/module/json_module.py
+++ src/module/json_module.py
@@ -3,16 +3,24 @@ import src.module.baseModule
 import json
 from StringIO import StringIO
 
+
+
 class baseModule(object):
     def __init__(self):
         print 'base module'
         pass
     pass
 
-class json_module(baseModule):
 
+class json_module(baseModule):
+    '''
+    this is pc json standard
+    The json must be have status and data
+    status is a str
+    data was dict
+    '''
     def __init__(self):
-        self.data={}
+        self.data = {}
 
     def __str__(self):
         '''
@@ -20,17 +28,47 @@ class json_module(baseModule):
         '''
         return "json_module"
 
-    def set(self,key,param):
+    def setData(self, key, param):
         '''
         :param key:
         :param param:
         :return:
         '''
-        if type(param) in (str,list,dict):
-            self.data[key]=param
+        if type(param) in (str, list, dict):
+            if key is 'data':
+                self.data[key] = param
         return self
         pass
-    def add(self,key,param):
+
+    def setStatus(self, key, param):
+        '''
+        :param key:
+        :param param:
+        :return:
+        '''
+        if type(param) in (str, list, dict):
+            if key is 'status':
+                self.data[key] = param
+        return self
+        pass
+
+    def set(self, key, param):
+        '''
+        :param key:
+        :param param:
+        :return:
+        '''
+        if type(param) in (str, list, dict):
+            self.data[key] = param
+        return self
+        pass
+
+    def add(self, key, param):
+        '''
+        :param key:
+        :param param:
+        :return:
+        '''
         self.data[key].append(param)
 
     def get(self):
